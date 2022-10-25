d32c800373e74ec89d4df8536a31f4f3ff5afa70
Bakarijack <bakarikilu254@gmail.com>
2022-10-25 11:18:14 +0300

    adding decription attribute and footer in base.html file

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+class Flower(models.Model):
+    title = models.CharField(max_length = 255, default = '')
+    description = models.TextField(default='')
+
+    def __str__(self) -> str:
+        return self.title
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from myapp.models import Flower
 
 # Create your views here.
 def index(request):
-    return render(request, "myapp/index.html")
+    flowers = Flower.objects.all()
+    return render(request, "myapp/index.html", {'flowers': flowers})
