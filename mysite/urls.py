6e56303256e6784895d4a56d1c5dbb81d3122c86
Bakarijack <bakarikilu254@gmail.com>
2022-10-25 13:35:25 +0300

    adding slug for links

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,6 @@ from myapp import views as myapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('flower/<int : id>/',myapp_views.detail, name='detail'),
+    path('flower/<slug:slug>/',myapp_views.detail, name='detail'),
     path('',myapp_views.index, name='index'),
 ]
