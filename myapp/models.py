274a64be1e3285e5582a066f707360b85a4186b3
Bakarijack <bakarikilu254@gmail.com>
2022-10-27 09:52:42 +0300

    create a searc feature

--- myapp/models.py
+++ myapp/models.py
@@ -7,10 +7,14 @@ from django.urls import reverse
 # Create your models here.
 class Tag(models.Model):
     title = models.CharField(max_length=255, default='')
+    slug = models.SlugField(blank=True, default = '')
 
     def __str__(self) -> str:
         return self.title
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Tag, self).save()
 
 class Category(models.Model):
     title = models.CharField(max_length = 255, default = '')
--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,20 @@ from myapp.models import Flower
 
 # Create your views here.
 def index(request):
-    flowers = Flower.objects.all()
+    #flowers = Flower.objects.all()
+    q = request.GET.get('q', None)
+    items = ''
+
+    if q is None or q is '':
+        flowers = Flower.objects.all()
+    elif q is not None:
+        flowers = Flower.objects.filter(title__contains = q)
     return render(request, "myapp/index.html", {'flowers': flowers})
 
 def detail(request, slug=None):               #stores the slug from the url into the variable slug or None if the value is not passed into this parameter
     flower = get_object_or_404(Flower, slug=slug)         #stores the object with the slug into the variable flower if it is found, else it returns error 404
     return render(request, 'myapp/detail.html', {'flower': flower})
+
+def tags(request, slug=None):
+    flowers = Flower.objects.filter(tags__slug = slug)
+    return render(request, 'myapp/index.html',{'flowers': flowers})
