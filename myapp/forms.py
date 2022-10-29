a2fdabff41c3ab144b9a7bf67f7b52535f5cb90e
Bakarijack <bakarikilu254@gmail.com>
2022-10-29 07:00:49 +0300

    implement custome

--- myapp/forms.py
+++ myapp/forms.py
@@ -4,6 +4,7 @@ from .models import Flower
 
 class MyForm(forms.ModelForm):
     title = forms.CharField(label="Title:", widget=forms.TextInput(attrs={'class':'form-control'}))
+    description = forms.CharField(label="Description", widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = Flower
